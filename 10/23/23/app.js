a62134529a5113129c8db91e59f8e6f3048b4ff6
Graehm <gpatrellfazio@gmail.com>
2023-10-23 22:20:54 -0500

    10/23

---
+++ 10/23/23/app.js
@@ -0,0 +1,6 @@
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+
+//p:a number
+//r: return a string fizzbuzz, fizz,or buzz
+//e: 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz fizz 13 14 fizzbuzz
+//p: create a loop from 1 to input n then for each iteration check if number is first divisible by both 3,5 if so log ("fizzbuzz") else if is divisible by 3 log (fizz) else if divisible by 5 log(fizzbuzz)
