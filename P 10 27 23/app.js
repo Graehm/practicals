5b149fcf4dddfdae410cac3a4ab036ef1635a8b0
Graehm <gpatrellfazio@gmail.com>
2023-10-27 16:01:32 -0500

    w2hw

---
+++ P 10 27 23/app.js
@@ -0,0 +1,44 @@
+// Create a clickable button that changes color and displays a message when clicked.
+
+// Instructions
+// Go to Codesandbox.io and create a new sandbox.
+// Add a new HTML file called index.htmlto your sandbox.
+// Inside the bodyelement of index.html, create a buttonelement with the text "Click Me!".
+// Add a CSS style to the buttonelement that sets its background color to blue and text color to white.
+// Add a JavaScript event listener to the buttonelement that changes the background color to green and displays the message "Button clicked!" when the button is clicked.
+// Requirements
+// The button should be centered horizontally on the page.
+// The button should have a minimum width of 200 pixels and a height of 50 pixels.
+// The button should have a cursor that changes to a pointer when hovered over.
+// The button should change color and display the message "Button clicked!" when clicked.
+// The HTML, CSS, and JS should be properly formatted and commented.
+// Tips
+// You can use the text-alignproperty in CSS to center the button horizontally on the page.
+// You can use the cursorproperty in CSS to change the cursor to a pointer when hovering over the button.
+// You can use the addEventListener()method in JavaScript to add a click event listener to the button.
+// You can use the styleproperty in JavaScript to change the button's background color and display a message.
+
+
+// // ----------------------------------------------------//
+
+
+// Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for multiples of five, print "Buzz". For numbers that are multiples of both three and five, print "FizzBuzz".
+
+// Instructions
+// Go to CodePen and create a new pen.
+// Create a new JavaScript file by clicking on the "JS" tab at the bottom of the screen.
+// Write a JavaScript program that prints the numbers from 1 to 100 following the rules above.
+// Test your program by running it using the "Console" feature in CodePen.
+// Submit your CodePen URL to your instructor.
+// Requirements
+// The program should print the numbers from 1 to 100.
+// For multiples of three, the program should print "Fizz" instead of the number.
+// For multiples of five, the program should print "Buzz" instead of the number.
+// For numbers that are multiples of both three and five, the program should print "FizzBuzz".
+// The program should be written in JavaScript and should be submitted as a CodePen URL.
+// The program should be properly formatted and commented.
+// Tips
+// You can use a loop to iterate through the numbers from 1 to 100.
+// You can use the modulus operator (%) to determine if a number is divisible by another number.
+// You can use the console.log()method to print output to the console.
+
