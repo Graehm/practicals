edb925095910a4768518045935646f7dead9780d
Graehm <gpatrellfazio@gmail.com>
2023-10-27 16:04:33 -0500

    w2

---
+++ P W2/app.js
@@ -0,0 +1,3 @@
+// Go to CodeSandbox.io. Click "Create Sandbox" in the lower right corner. Choose the "Static" template. Create styles.css, and index.js files ready for you to work on and properly link them.
+
+// Step 2: Set up the HTML structure
